0c4869b171521c56a2da36209dcc264af3e4562b
pilibottazzi <pbottazzi@neix.com.ar>
2026-02-24 13:14:16 -0300

    Update encuesta.py

--- tools/MKT/encuesta.py
+++ tools/MKT/encuesta.py
@@ -4,7 +4,6 @@ import streamlit.components.v1 as components
 FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSemUHA9xQP8iMkeBf-DkNl8QKJ_2gqToDzXd9k72VYhbe0IDw/viewform?usp=sharing&ouid=111449706457807508532"
 
 def _to_embed(url: str) -> str:
-    # Google Forms embed: reemplazar /viewform por /viewform?embedded=true
     if "embedded=true" in url:
         return url
     if "viewform" in url:
@@ -18,14 +17,6 @@ def render(_=None):
         "<div class='section-sub'>Cargar gasto desde el formulario (queda registrado automáticamente en Google Sheets)</div>",
         unsafe_allow_html=True,
     )
-
-    c1, c2 = st.columns([1, 1])
-    with c1:
-        st.link_button("Abrir formulario en una pestaña nueva", FORM_URL)
-    with c2:
-        st.caption("Si no se ve el embed, usá el botón de abrir arriba.")
-
-    st.divider()
 
     # Embed
     embed_url = _to_embed(FORM_URL)
